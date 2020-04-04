5a068ac435dbbb585bce560f5d775d597937f4ee
Dora Belme <contact.dorabelme@gmail.com>
2020-04-04 18:37:00 -0400

    autocomplete widget

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get("http://www.omdbapi.com/", {
         params: {
             apikey: '1dbb59c1',
-            i: 'tt0848228'
+            s: searchTerm
         }
     });
     console.log(response.data);
 };
 
-fetchData();
+
+const input = document.querySelector("input");
+
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay)
+    };
+};
+
+const onInput = event => {
+    fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 500));
+
+/* SIMPLE SOLUTION */
+
+// let timeoutId;
+// const onInput = event => {
+//     if (timeoutId) {
+//         clearTimeout(timeoutId);
+//     }
+//     timeoutId = setTimeout(() => {
+//         fetchData(event.target.value)
+//     }, 500)
+// }
+// input.addEventListener("input", onInput);
